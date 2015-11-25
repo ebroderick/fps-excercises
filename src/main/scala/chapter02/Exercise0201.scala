b257745e9b4000bd0d0a1b890370949b3bb8e47b
ebroderick <evanbrod@gmail.com>
2015-11-25 07:49:40 -0500

    starting chp 2

--- src/main/scala/chapter02/Exercise0201.scala
+++ src/main/scala/chapter02/Exercise0201.scala
@@ -4,7 +4,16 @@ import scala.annotation.tailrec
 
 object Exercise0201 {
 
+  def fib(number: Int): Int = {
 
+    @tailrec
+    def go(i: Int, prev: Int, current: Int): Int = {
+      if (i == 0) prev
+      else go(i - 1, current, prev + current)
+    }
+
+    go(number, 0, 1)
+  }
 }
 
 
